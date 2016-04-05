fa9a5ce2f68596c9c15b5fc2815b3619d8cf4ee6
xiepengchong <xiepengchong@qiyi.com>
2016-04-05 09:44:31 +0800

    标题：更换服务器地址 原因：局域网内，ip可能会变化

--- my-server/etc/app.config.php
+++ my-server/etc/app.config.php
@@ -33,8 +33,8 @@ define('__APP_VERSION', '1.0');
 /**
  * URL relative constants
  */
-define('__HOST_SERVER', 'http://192.168.1.2:8001');
-define('__HOST_WEBSITE', 'http://192.168.1.2:8002');
+define('__HOST_SERVER', 'http://10.101.66.158:888');
+define('__HOST_WEBSITE', 'http://10.101.66.158:888');
 
 /**
  * MVC url mapping ini file
