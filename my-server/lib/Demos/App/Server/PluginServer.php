31a88384c4f251836662a7a4e179edcaa3658b4b
xiepengchong <pc.xie01@zuche.com>
2016-04-30 09:56:33 +0800

    标题：添加插件列表进入后台调试列表中 原因：列表中需要添加注释才能显示，backup为不能显示的示例，暂时未找到原因，先记录下来

--- my-server/lib/Demos/App/Server/PluginServer.php
+++ my-server/lib/Demos/App/Server/PluginServer.php
@@ -1,17 +1,51 @@
 <?php
-/*
- * Created on 2016年4月17日
+/**
+ * Demos App
  *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
+ * @category   Demos
+ * @package    Demos_App_Server
+ * @author     James.Huang <huangjuanshi@163.com>
+ * @license    http://www.apache.org/licenses/LICENSE-2.0
+ * @version    $Id$
  */
- require_once 'Demos/App/Server.php';
- require_once 'Demos/Util/plugin.php';
- 
- 
+
+require_once 'Demos/App/Server.php';
+require_once 'Demos/Util/plugin.php';
+
+/**
+ * @package Demos_App_Server
+ */
 class PluginServer extends Demos_App_Server
 {
-	public function pluginListAction(){
+	/**
+	 * ---------------------------------------------------------------------------------------------
+	 * > 全局设置：
+	 * <code>
+	 * </code>
+	 * ---------------------------------------------------------------------------------------------
+	 */
+	public function __init ()
+	{
+		parent::__init();
+	}
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	// service api methods
+	
+	/**
+	 * ---------------------------------------------------------------------------------------------
+	 * > 接口说明：获取插件列表
+	 * <code>
+	 * URL地址：/plugin/pluginList
+	 * 提交方式：GET
+	 * </code>
+	 * ---------------------------------------------------------------------------------------------
+	 * @title 获取插件列表
+	 * @action /plugin/pluginList
+	 * @method get
+	 */
+	public function pluginListAction ()
+	{
 		$faceIdArr = range(0,0);
 		// get  images
 		$faceList = array();
@@ -23,4 +57,3 @@ class PluginServer extends Demos_App_Server
 		));
 	}
 }
-?>
---
+++ my-server/lib/Demos/App/Server/PluginServerBackup.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Demos App
+ *
+ * @category   Demos
+ * @package    Demos_App_Server
+ * @author     James.Huang <huangjuanshi@163.com>
+ * @license    http://www.apache.org/licenses/LICENSE-2.0
+ * @version    $Id$
+ */
+
+require_once 'Demos/App/Server.php';
+require_once 'Demos/Util/plugin.php';
+
+/**
+ * @package Demos_App_Server
+ */
+class PluginServerBackup extends Demos_App_Server
+{
+	/**
+	 * ---------------------------------------------------------------------------------------------
+	 * > 全局设置：
+	 * <code>
+	 * </code>
+	 * ---------------------------------------------------------------------------------------------
+	 */
+	public function __init ()
+	{
+		parent::__init();
+	}
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	// service api methods
+	
+	/**
+	 * ---------------------------------------------------------------------------------------------
+	 * > 接口说明：获取插件列表
+	 * <code>
+	 * URL地址：/plugin/pluginList
+	 * 提交方式：GET
+	 * </code>
+	 * ---------------------------------------------------------------------------------------------
+	 * @title 获取插件列表
+	 * @action /plugin/pluginList
+	 * @method get
+	 */
+	public function pluginListAction ()
+	{
+		$faceIdArr = range(0,0);
+		// get  images
+		$faceList = array();
+		foreach ($faceIdArr as $faceId) {
+			$faceList[] = Demo_Util_Plugin::getPluginObject("demos.app.com.plugindemo");
+		}
+		$this->render('10000', 'Get plugin list ok', array(
+			'Plugin.list' => $faceList
+		));
+	}
+}
